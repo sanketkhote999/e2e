fe3c288bcd547d4c5612f9813a65989521da7cc1
Sanket Khote <sanket.khote@skeletos.in>
2024-10-23 18:52:48 +0530

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import Dashboard from '@/components/Dashboard';
+import Dashboard from "@/components/Dashboard";
 
+// TODO: Add a loading state
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
